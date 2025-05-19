589f542041b461058d08237b131bfce55bfcfeaa
Nikij <niki.heartj@gmail.com>
2025-05-19 23:20:14 +0300

    Update default BaseURL in workflow and tests to simplify configuration

--- tests/no-new-game-navbar.test.js
+++ tests/no-new-game-navbar.test.js
@@ -2,9 +2,9 @@ import { test, expect } from "@playwright/test";
 
 test("New game is missing in navbar", async ({ page }) => {
   const GAME = "ZZZ";
-  await page.goto("https://mobalytics.gg/poe-2", {
-    waitUntil: "domcontentloaded",
-  });
+  await page.goto("");
+  console.log(await page.url());
+  await page.getByRole("link", { name: "5c7b868f-logo-diablo-" }).click();
   await expect(page.getByText("LoLTFTVALLost ArkDestiny")).not.toContainText(
     GAME
   );
