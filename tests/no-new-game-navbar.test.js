7abba69e43b099d15c903406d101854fb3252e8a
Nikij <niki.heartj@gmail.com>
2025-05-22 15:48:54 +0300

    Remove unnecessary options from page.goto in navbar test

--- tests/no-new-game-navbar.test.js
+++ tests/no-new-game-navbar.test.js
@@ -2,9 +2,7 @@ import { test, expect } from "@playwright/test";
 
 test("There is no a new game in navbar on the produciton", async ({ page }) => {
   const GAME = "ZZZ";
-  await page.goto("", {
-    waitUntil: "domcontentloaded",
-  });
+  await page.goto("");
   await page.getByRole("link", { name: "5c7b868f-logo-diablo-" }).click();
   await expect(page.getByText("LoLTFTVALLost ArkDestiny")).not.toContainText(
     GAME
