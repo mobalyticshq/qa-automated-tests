2694579c062bf56635214e68f4ac45fddec75507
Nikij <niki.heartj@gmail.com>
2025-05-23 15:09:26 +0300

    added test-steps to tests

--- tests/ngf.test.js
+++ tests/ngf.test.js
@@ -1,13 +1,20 @@
 import { test, expect } from "@playwright/test";
 
-test("There is no a new game in navbar on the produciton", async ({ page }) => {
+test("There is no a new game in navbar on the produciton", async ({
+  page,
+  baseURL,
+}) => {
   const NEW_GAME = "ZZZ";
-
-  await page.goto("", {
-    waitUntil: "domcontentloaded",
+  await test.step(`Go to ${baseURL}`, async () => {
+    await page.goto("/", { waitUntil: "domcontentloaded" });
   });
-  await page.getByRole("link", { name: "5c7b868f-logo-diablo-" }).click();
-  await expect(page.getByText("LoLTFTVALLost ArkDestiny")).not.toContainText(
-    NEW_GAME
-  );
+  await test.step("Click on the D4 game", async () => {
+    await page.getByRole("link", { name: "5c7b868f-logo-diablo-" }).click();
+  });
+
+  await test.step(`Expected Result: There is no ${NEW_GAME} in the navbar on prod`, async () => {
+    await expect(page.getByText("LoLTFTVALLost ArkDestiny")).not.toContainText(
+      NEW_GAME
+    );
+  });
 });
--- tests/sitemap.test.js
+++ tests/sitemap.test.js
@@ -1,134 +1,198 @@
 import { test, expect } from "@playwright/test";
 
 const PROD_SITEMAP = "https://mobalytics.gg/sitemap.xml";
-const NEW_GAME_LINK = "https://mobalytics.gg/zzz/sitemap.xml";
+const NEW_GAME = "https://mobalytics.gg/zzz/sitemap.xml";
 
-test(`There is no a new game: ${NEW_GAME_LINK} in the: ${PROD_SITEMAP} list`, async ({
+test(`There is no a new game ${NEW_GAME} in the ${PROD_SITEMAP}`, async ({
   page,
 }) => {
-  await page.goto(PROD_SITEMAP);
-  await expect(page.locator("#folder0")).not.toContainText(NEW_GAME_LINK);
+  await test.step(`Go to ${PROD_SITEMAP}`, async () => {
+    await page.goto(PROD_SITEMAP);
+  });
+});
+
+await test.step(`Expected Result: There is no ${NEW_GAME} in the ${PROD_SITEMAP}`, async () => {
+  await expect(page.locator("#folder0")).not.toContainText(NEW_GAME);
 });
 
 const SITEMAP_INDEX = "https://mobalytics.gg/sitemap_index.xml";
 
-test(`${SITEMAP_INDEX} is present in the: ${PROD_SITEMAP} list`, async ({
-  page,
-}) => {
-  await page.goto(PROD_SITEMAP);
-  await expect(page.locator("#folder0")).toContainText(SITEMAP_INDEX);
+test(`${SITEMAP_INDEX} is present in the ${PROD_SITEMAP}`, async ({ page }) => {
+  await test.step(`Go to ${PROD_SITEMAP}`, async () => {
+    await page.goto(PROD_SITEMAP);
+  });
+
+  await test.step(`Expected Result: ${SITEMAP_INDEX} is present in the ${PROD_SITEMAP}`, async () => {
+    await expect(page.locator("#folder0")).toContainText(SITEMAP_INDEX);
+  });
 });
 
 const PRODUCT_SITEMAP = "https://mobalytics.gg/product-sitemap.xml";
 
-test(`${PRODUCT_SITEMAP} is present in the: ${PROD_SITEMAP} list`, async ({
+test(`${PRODUCT_SITEMAP} is present in the ${PROD_SITEMAP}`, async ({
   page,
 }) => {
-  await page.goto(PROD_SITEMAP);
-  await expect(page.locator("#folder0")).toContainText(PRODUCT_SITEMAP);
+  await test.step(`Go to ${PROD_SITEMAP}`, async () => {
+    await page.goto(PROD_SITEMAP);
+  });
+
+  await test.step(`Expected Result: ${PRODUCT_SITEMAP} is present in the ${PROD_SITEMAP}`, async () => {
+    await expect(page.locator("#folder0")).toContainText(PRODUCT_SITEMAP);
+  });
 });
 
 const VALORANT = "https://mobalytics.gg/valorant/sitemap.xml";
 
-test(`${VALORANT} is present in the: ${PROD_SITEMAP} list`, async ({
-  page,
-}) => {
-  await page.goto(PROD_SITEMAP);
-  await expect(page.locator("#folder0")).toContainText(VALORANT);
+test(`${VALORANT} is present in the ${PROD_SITEMAP}`, async ({ page }) => {
+  await test.step(`Go to ${PROD_SITEMAP}`, async () => {
+    await page.goto(PROD_SITEMAP);
+  });
+
+  await test.step(`Expected Result: ${VALORANT} is present in the ${PROD_SITEMAP}`, async () => {
+    await expect(page.locator("#folder0")).toContainText(VALORANT);
+  });
 });
 
 const TFT = "https://mobalytics.gg/tft/sitemap.xml";
 
-test(`${TFT} is present in the: ${PROD_SITEMAP} list`, async ({ page }) => {
-  await page.goto(PROD_SITEMAP);
-  await expect(page.locator("#folder0")).toContainText(TFT);
+test(`${TFT} is present in the ${PROD_SITEMAP}`, async ({ page }) => {
+  await test.step(`Go to ${PROD_SITEMAP}`, async () => {
+    await page.goto(PROD_SITEMAP);
+  });
+
+  await test.step(`Expected Result: ${TFT} is present in the ${PROD_SITEMAP}`, async () => {
+    await expect(page.locator("#folder0")).toContainText(TFT);
+  });
 });
 
 const SET14 = "https://mobalytics.gg/tft/set14/sitemap.xml";
 
-test(`${SET14} is present in the: ${PROD_SITEMAP} list`, async ({ page }) => {
-  await page.goto(PROD_SITEMAP);
-  await expect(page.locator("#folder0")).toContainText(SET14);
+test(`${SET14} is present in the ${PROD_SITEMAP}`, async ({ page }) => {
+  await test.step(`Go to ${PROD_SITEMAP}`, async () => {
+    await page.goto(PROD_SITEMAP);
+  });
+
+  await test.step(`Expected Result: ${SET14} is present in the ${PROD_SITEMAP}`, async () => {
+    await expect(page.locator("#folder0")).toContainText(SET14);
+  });
 });
 
 const SET10 = "https://mobalytics.gg/tft/set10/sitemap.xml";
 
-test(`${SET10} is present in the: ${PROD_SITEMAP} list`, async ({ page }) => {
-  await page.goto(PROD_SITEMAP);
-  await expect(page.locator("#folder0")).toContainText(SET10);
+test(`${SET10} is present in the ${PROD_SITEMAP}`, async ({ page }) => {
+  await test.step(`Go to ${PROD_SITEMAP}`, async () => {
+    await page.goto(PROD_SITEMAP);
+  });
+
+  await test.step(`Expected Result: ${SET10} is present in the ${PROD_SITEMAP}`, async () => {
+    await expect(page.locator("#folder0")).toContainText(SET10);
+  });
 });
 
 const LOST_ARK = "https://mobalytics.gg/lost-ark/sitemap.xml";
 
-test(`${LOST_ARK} is present in the: ${PROD_SITEMAP} list`, async ({
-  page,
-}) => {
-  await page.goto(PROD_SITEMAP);
-  await expect(page.locator("#folder0")).toContainText(LOST_ARK);
+test(`${LOST_ARK} is present in the ${PROD_SITEMAP}`, async ({ page }) => {
+  await test.step(`Go to ${PROD_SITEMAP}`, async () => {
+    await page.goto(PROD_SITEMAP);
+  });
+
+  await test.step(`Expected Result: ${LOST_ARK} is present in the ${PROD_SITEMAP}`, async () => {
+    await expect(page.locator("#folder0")).toContainText(LOST_ARK);
+  });
 });
 
 const DESTINY_2 = "https://mobalytics.gg/destiny-2/sitemap.xml";
 
-test(`${DESTINY_2} is present in the: ${PROD_SITEMAP} list`, async ({
-  page,
-}) => {
-  await page.goto(PROD_SITEMAP);
-  await expect(page.locator("#folder0")).toContainText(DESTINY_2);
+test(`${DESTINY_2} is present in the ${PROD_SITEMAP}`, async ({ page }) => {
+  await test.step(`Go to ${PROD_SITEMAP}`, async () => {
+    await page.goto(PROD_SITEMAP);
+  });
+
+  await test.step(`Expected Result: ${DESTINY_2} is present in the ${PROD_SITEMAP}`, async () => {
+    await expect(page.locator("#folder0")).toContainText(DESTINY_2);
+  });
 });
 
 const DIABLO_4 = "https://mobalytics.gg/diablo-4/sitemap.xml";
 
-test(`${DIABLO_4} is present in the: ${PROD_SITEMAP} list`, async ({
-  page,
-}) => {
-  await page.goto(PROD_SITEMAP);
-  await expect(page.locator("#folder0")).toContainText(DIABLO_4);
+test(`${DIABLO_4} is present in the ${PROD_SITEMAP}`, async ({ page }) => {
+  await test.step(`Go to ${PROD_SITEMAP}`, async () => {
+    await page.goto(PROD_SITEMAP);
+  });
+
+  await test.step(`Expected Result: ${DIABLO_4} is present in the ${PROD_SITEMAP}`, async () => {
+    await expect(page.locator("#folder0")).toContainText(DIABLO_4);
+  });
 });
 
 const ELDEN_RING = "https://mobalytics.gg/elden-ring-nightreign/sitemap.xml";
 
-test(`${ELDEN_RING} is present in the: ${PROD_SITEMAP} list`, async ({
-  page,
-}) => {
-  await page.goto(PROD_SITEMAP);
-  await expect(page.locator("#folder0")).toContainText(ELDEN_RING);
+test(`${ELDEN_RING} is present in the ${PROD_SITEMAP}`, async ({ page }) => {
+  await test.step(`Go to ${PROD_SITEMAP}`, async () => {
+    await page.goto(PROD_SITEMAP);
+  });
+
+  await test.step(`Expected Result: ${ELDEN_RING} is present in the ${PROD_SITEMAP}`, async () => {
+    await expect(page.locator("#folder0")).toContainText(ELDEN_RING);
+  });
 });
 
 const MARVEL_RIVALS = "https://mobalytics.gg/marvel-rivals/sitemap.xml";
 
-test(`${MARVEL_RIVALS} is present in the: ${PROD_SITEMAP} list`, async ({
-  page,
-}) => {
-  await page.goto(PROD_SITEMAP);
-  await expect(page.locator("#folder0")).toContainText(MARVEL_RIVALS);
+test(`${MARVEL_RIVALS} is present in the ${PROD_SITEMAP}`, async ({ page }) => {
+  await test.step(`Go to ${PROD_SITEMAP}`, async () => {
+    await page.goto(PROD_SITEMAP);
+  });
+
+  await test.step(`Expected Result: ${MARVEL_RIVALS} is present in the ${PROD_SITEMAP}`, async () => {
+    await expect(page.locator("#folder0")).toContainText(MARVEL_RIVALS);
+  });
 });
 
 const MHV = "https://mobalytics.gg/mhw/sitemap.xml";
 
-test(`${MHV} is present in the: ${PROD_SITEMAP} list`, async ({ page }) => {
-  await page.goto(PROD_SITEMAP);
-  await expect(page.locator("#folder0")).toContainText(MHV);
+test(`${MHV} is present in the ${PROD_SITEMAP}`, async ({ page }) => {
+  await test.step(`Go to ${PROD_SITEMAP}`, async () => {
+    await page.goto(PROD_SITEMAP);
+  });
+
+  await test.step(`Expected Result: ${MHV} is present in the ${PROD_SITEMAP}`, async () => {
+    await expect(page.locator("#folder0")).toContainText(MHV);
+  });
 });
 
 const DEADLOCK = "https://mobalytics.gg/deadlock/sitemap.xml";
 
-test(`${DEADLOCK} is present in the: ${PROD_SITEMAP} list`, async ({
-  page,
-}) => {
-  await page.goto(PROD_SITEMAP);
-  await expect(page.locator("#folder0")).toContainText(DEADLOCK);
+test(`${DEADLOCK} is present in the ${PROD_SITEMAP}`, async ({ page }) => {
+  await test.step(`Go to ${PROD_SITEMAP}`, async () => {
+    await page.goto(PROD_SITEMAP);
+  });
+
+  await test.step(`Expected Result: ${DEADLOCK} is present in the ${PROD_SITEMAP}`, async () => {
+    await expect(page.locator("#folder0")).toContainText(DEADLOCK);
+  });
 });
 
 const POE_2 = "https://mobalytics.gg/poe-2/sitemap.xml";
 
-test(`${POE_2} is present in the: ${PROD_SITEMAP} list`, async ({ page }) => {
-  await page.goto(PROD_SITEMAP);
-  await expect(page.locator("#folder0")).toContainText(POE_2);
+test(`${POE_2} is present in the ${PROD_SITEMAP}`, async ({ page }) => {
+  await test.step(`Go to ${PROD_SITEMAP}`, async () => {
+    await page.goto(PROD_SITEMAP);
+  });
+
+  await test.step(`Expected Result: ${POE_2} is present in the ${PROD_SITEMAP}`, async () => {
+    await expect(page.locator("#folder0")).toContainText(POE_2);
+  });
 });
 
 const BAZAAR = "https://mobalytics.gg/the-bazaar/sitemap.xml";
 
-test(`${BAZAAR} is present in the: ${PROD_SITEMAP} list`, async ({ page }) => {
-  await page.goto(PROD_SITEMAP);
-  await expect(page.locator("#folder0")).toContainText(BAZAAR);
+test(`${BAZAAR} is present in the ${PROD_SITEMAP}`, async ({ page }) => {
+  await test.step(`Go to ${PROD_SITEMAP}`, async () => {
+    await page.goto(PROD_SITEMAP);
+  });
+
+  await test.step(`Expected Result: ${BAZAAR} is present in the ${PROD_SITEMAP}`, async () => {
+    await expect(page.locator("#folder0")).toContainText(BAZAAR);
+  });
 });
