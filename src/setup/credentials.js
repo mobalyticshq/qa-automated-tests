4bf6eb3efe8667f1140399dd3f5d4fe1136c54c6
Nikij <niki.heartj@gmail.com>
2025-05-26 21:26:30 +0300

    feat: Update environment variables and credentials to use secrets for sensitive information

--- src/setup/credentials.js
+++ src/setup/credentials.js
@@ -1,14 +1,14 @@
 export const USER_ROLES = {
   admin_prod: {
-    email: "admin_auto_tests+ns@mobalyticshq.com",
-    password: "admin_auto_tests+ns@mobalyticshq.com",
+    email: process.env.ADMIN_EMAIL,
+    password: process.env.ADMIN_PASSWORD,
   },
   game_manager_prod: {
-    email: "game_manager_auto_tests+ns@mobalyticshq.com",
-    password: "game_manager_auto_tests+ns@mobalyticshq.com",
+    email: process.env.GAME_MANAGER_EMAIL,
+    password: process.env.GAME_MANAGER_PASSWORD,
   },
   internal_writer_prod: {
-    email: "internal_writer_auto_tests+ns@mobalyticshq.com",
-    password: "internal_writer_auto_tests+ns@mobalyticshq.com",
+    email: process.env.INTERNAL_WRITER_EMAIL,
+    password: process.env.INTERNAL_WRITER_PASSWORD,
   },
 };
