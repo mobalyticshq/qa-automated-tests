c22914648a41c0d3c99b4853c769f838817198a3
Vlad <vladislav.trushkov@mobalyticshq.com>
2026-09-18 16:58:07 +0400

    feat(tests): implement test for opening and closing the perimeter map modal

--- e2e-tests/ngf/widgets.test.js
+++ e2e-tests/ngf/widgets.test.js
@@ -1,7 +1,14 @@
 import { test, expect } from '../fixtures/fixture';
+import { Moba } from '../../app/page-object/moba';
 
 test.use({ storageState: { cookies: [], origins: [] } });
 
-test('Widgets test', async ({ page }) => {
-    await page.goto('https://stg.mobalytics.gg/tft');
+test('User can open and close the perimeter map modal', async ({ page }) => {
+    const moba = new Moba(page);
+
+    await moba.mainURLs.openMarathonPerimeterMapPage();
+    await moba.stPage.perimeterMapButton.click();
+    await expect(moba.stPage.closeMapModalButton).toBeVisible();
+    await moba.stPage.closeMapModalButton.click();
+    await expect(moba.stPage.closeMapModalButton).not.toBeVisible();
 });
